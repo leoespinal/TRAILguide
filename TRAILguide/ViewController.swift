dc166b1e26afe89bd0aec6ac3da3b91327dccc35
Leo Espinal <leo.espinal420@gmail.com>
2018-03-18 22:51:27 -0400

    Initial Commit

---
+++ TRAILguide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TRAILguide
+//
+//  Created by Leo Espinal on 3/18/18.
+//  Copyright © 2018 Leo Espinal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
