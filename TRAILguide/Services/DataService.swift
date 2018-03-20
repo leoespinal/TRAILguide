ce3ad4ccfd1b88f1cd768f556c6cdac15a118399
Leo Espinal <leo.espinal420@gmail.com>
2018-03-20 11:35:54 -0400

    Added README.md and app screenshots

--- TRAILguide/Services/DataService.swift
+++ TRAILguide/Services/DataService.swift
@@ -25,7 +25,7 @@ class DataService {
     //Product Data
     private let campingProducts = [
         Product(_name: "SLEEPING BAG", _description: "A sleeping bag is a camping essential. It provides a warm and semi-comfortable place to sleep when camping. Some sleeping bags are rated for temperatures below 0 degrees F! We recommend the SnugSleeper SubZero bag for camping anywhere you think you might be cold.", _imageName: "sleepingBag.png"),
-        Product(_name: "CAMPING STOVE", _description: "A camping stove is a really nice addition to the camping experience. Forget about struggling to start a fire in a local camp grill, just fire up the Cole Burner 2018 which is our top rated propane stove.", _imageName: "campingStove.png"),
+        Product(_name: "CAMPING STOVE", _description: "A camping stove is a really nice addition to the camping experience. Forget about struggling to start a fire in a local camp grill, just fire up the Cole Burner 2018 which is a top rated propane stove.", _imageName: "campingStove.png"),
         Product(_name: "TENT", _description: "A tent is definitely a must have when camping. It provides shelter during an overnight stay on any campsite. On nights with high winds we recommend the Windsor Vising Tent which keeps very sturdy during highwinds.", _imageName: "pupTent.png"),
         Product(_name: "COOLER", _description: "A spacicious cooler for all of your food and beverages are an absolute must on any camping trip. With this 60\" x 60\" cooler you definitely can't go wrong and have plenty of space to store and keep your food cool for up to 72 hours.", _imageName: "foodCooler.png"),
         Product(_name: "PARACORD BRACLET", _description: "An essential camping survival tool.", _imageName: "paracordBracelet.png"),
